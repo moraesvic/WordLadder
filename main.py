69970a23077f319df7402231e609d456647affce
Michael <vitao.moraes@gmail.com>
2020-09-06 13:00:07 -0300

    a

---
+++ main.py
@@ -0,0 +1,15 @@
+a
+b
+class whatever_shit(object):
+	"""docstring for whatever_shit"""
+	def __init__(self, arg):
+		super(whatever_shit, self).__init__()
+		self.arg = arg
+		
+def my_function(arg1, arg2):
+	pass
+
+def main():
+
+	pass
+
